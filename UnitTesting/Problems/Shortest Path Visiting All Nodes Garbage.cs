67468370e39395908adeabcb3af3cb839f770656
Mateusz Kopij <sagasu@gmail.com>
2022-02-27 21:51:05 +0000

    Adding code examples to elaborate how to unit test it.

---
+++ UnitTesting/Problems/Shortest Path Visiting All Nodes Garbage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTesting.Problems
+{
+
+    internal class Shortest_Path_Visiting_All_Nodes_Garbage
+    {
+        // https://leetcode.com/problems/shortest-path-visiting-all-nodes/
+        // Not my solution but true garbage
+        Dictionary<int, List<int>> _graph;
+        Dictionary<int, int> parentChild;
+        int min = int.MaxValue;
+
+        public int ShortestPathLength(int[][] graph)
+        {
+            _graph = new Dictionary<int, List<int>>();
+            parentChild = new Dictionary<int, int>();
+            var candidates = new List<int>();
+            min = int.MaxValue;
+            for (var i = 0; i < graph.Length; i++)
+            {
+                _graph.Add(i, new List<int>());
+                if (graph[i].Length == 1)
+                {
+                    candidates.Add(i);
+                }
+                _graph[i] = graph[i].ToList();
+            }
+            if (candidates.Count == 0)
+            {
+                return graph.Length - 1;
+            }
+            foreach (var i in candidates)
+            {
+                parentChild = new Dictionary<int, int>();
+                Dfs(-1, i, graph.Length, new HashSet<int>(), 0, false);
+
+            }
+            return min;
+        }
+
+
+        public bool Dfs(int parent, int current, int N, HashSet<int> seen, int cost, bool rec)
+        {
+            seen.Add(current);
+            if (!parentChild.ContainsKey(current))
+            {
+                parentChild.Add(current, parent);
+            }
+            if (seen.Count == N)
+            {
+                min = Math.Min(min, cost);
+                seen.Remove(current);
+                parentChild.Remove(current);
+                return true;
+            }
+            var res = false;
+            foreach (var nghr in _graph[current])
+            {
+                if (!seen.Contains(nghr) && nghr != parent)
+                {
+                    res |= Dfs(current, nghr, N, seen, cost + 1, false);
+                }
+            }
+            if (!res && parentChild.ContainsKey(parent))
+            {
+                res = Dfs(parentChild[parent], parent, N, seen, cost + 1, true);
+                if (!rec)
+                {
+                    seen.Remove(current);
+                }
+                parentChild.Remove(current);
+                return res;
+            }
+            if (!rec)
+            {
+                seen.Remove(current);
+                parentChild.Remove(current);
+            }
+            return res;
+        }
+    }
+}
---
+++ UnitTesting/Problems/Shortest Path Visiting All Nodes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTesting.Problems
+{
+
+    internal class Shortest_Path_Visiting_All_Nodes
+    {
+        // https://leetcode.com/problems/shortest-path-visiting-all-nodes/
+        // Not my solution
+        public int ShortestPathLength(int[][] graph)
+        {
+            int res = 0, fullMask = (1 << graph.Length) - 1;
+
+            HashSet<(int, int)> used = new();
+            Queue<(int mask, int v)> queue = new(Enumerable.Range(0, graph.Length).Select(i => (1 << i, i)));
+
+            while (queue.Any())
+            {
+                for (int count = queue.Count; count > 0; --count)
+                {
+                    var pos = queue.Dequeue();
+
+                    if (used.Contains(pos)) continue;
+                    if (pos.mask == fullMask) return res;
+
+                    used.Add(pos);
+                    foreach (int u in graph[pos.v]) queue.Enqueue((pos.mask | (1 << u), u));
+                }
+                ++res;
+            }
+
+            throw new ArgumentException("Graph isn't connected");
+        }
+    }
+}
